78dfac25a9f5d92a81c200dbf22c137b97bcd941
Ashish Dungdung <official.ashishd2@gmail.com>
2024-10-03 12:29:30 +0530

    style(SliderBtn): Implemented smooth slider movement when clicked

--- src/components/buttons/sliderBtn/SliderBtn.tsx
+++ src/components/buttons/sliderBtn/SliderBtn.tsx
@@ -1,11 +1,24 @@
 import styles from "./SliderBtn.module.scss";
 
+import { useState } from "react";
+
 const SliderBtn = function () {
+  const [sliderActive, setSliderActive] = useState(false);
+
+  const handleSliderClick = function () {
+    setSliderActive(!sliderActive);
+  };
+
   return (
     <button
       className={`${styles.sliderContainer} ${styles.sliderContainer_lt}`}
+      onClick={handleSliderClick}
     >
-      <span className={`${styles.slider} ${styles.slider_lt}`}></span>
+      <span
+        className={`${styles.slider} ${
+          sliderActive ? styles.slider_active : ""
+        } ${styles.slider_lt}`}
+      ></span>
     </button>
   );
 };
