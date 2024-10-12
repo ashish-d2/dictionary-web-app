e9dfa8d66692492225b60585a97a8bf2ded7458f
Ashish Dungdung <official.ashishd2@gmail.com>
2024-10-12 12:38:40 +0530

    style(Footer): Implemented Dark mode for footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,13 +2,20 @@ import styles from "./Footer.module.scss";
 
 import { ReactComponent as LinkIcon } from "./../../assets/images/icon-new-window.svg";
 
+import { useTheme } from "../../context/ThemeContext";
+
 const Footer = function () {
+  const { theme } = useTheme();
+
   return (
     <div className={styles.footer}>
       <div className={styles.divider}></div>
       <div className={`sansSerif ${styles.source_container}`}>
         <p className={styles.source}>Source</p>
-        <a href="#" className="light">
+        <a
+          href="#"
+          className={`${theme === "light" ? "text-light" : "text-dark"}`}
+        >
           https://en.wiktionary.org/wiki/keyboard
         </a>
         <LinkIcon className={styles.linkIcon} />
