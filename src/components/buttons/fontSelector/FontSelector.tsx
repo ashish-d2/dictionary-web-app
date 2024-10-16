8a807a3279148fe8befd24c8704ef6d3ada8caaa
Ashish Dungdung <official.ashishd2@gmail.com>
2024-10-16 21:53:52 +0530

    feat(FontSelector): Implemented styles for light theme mobile view

--- src/components/buttons/fontSelector/FontSelector.tsx
+++ src/components/buttons/fontSelector/FontSelector.tsx
@@ -1,33 +1,17 @@
-import { useRef } from "react";
-
 import styles from "./FontSelector.module.scss";
 
 import { ReactComponent as ArrowLogo } from "./../../../assets/images/icon-arrow-down.svg";
 
 const FontSelector = function () {
-  const selectRef = useRef<HTMLSelectElement>(null);
-
-  const handleArrowBtnClick = function () {
-    if (selectRef.current) {
-      selectRef.current.focus();
-      //   selectRef.current.click();
-    }
-  };
-
   return (
-    <div className={styles.fontSelector_wrapper}>
-      <select
-        className={`${styles.fontSelector} ${styles.fontSelector_sansSerif} ${styles.fontSelector_lt}`}
-        ref={selectRef}
-      >
-        <option value="sans-serif">Sans Serif</option>
-        <option value="serif">Serif</option>
-        <option value="mono">Mono</option>
-      </select>
-
-      <div className={styles.arrowBtn} onClick={handleArrowBtnClick}>
-        <ArrowLogo />
+    <div className={`${styles.fontSelector} text-light`}>
+      <p>Sans Serif</p>
+      <div className={`${styles.fonts}`}>
+        <p className="sans-Serif">Sans Serif</p>
+        <p className="serif">Serif</p>
+        <p className="mono">Mono</p>
       </div>
+      <ArrowLogo />
     </div>
   );
 };
