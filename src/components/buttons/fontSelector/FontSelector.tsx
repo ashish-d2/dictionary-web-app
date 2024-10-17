76f4aff4736936f2591d3dcfd48f01b15133657d
Ashish Dungdung <official.ashishd2@gmail.com>
2024-10-17 20:45:45 +0530

    style(FontSelector): Implemented styles for dark theme mobile view

--- src/components/buttons/fontSelector/FontSelector.tsx
+++ src/components/buttons/fontSelector/FontSelector.tsx
@@ -1,12 +1,24 @@
 import styles from "./FontSelector.module.scss";
 
+import { useTheme } from "../../../context/ThemeContext";
+
 import { ReactComponent as ArrowLogo } from "./../../../assets/images/icon-arrow-down.svg";
 
 const FontSelector = function () {
+  const { theme } = useTheme();
+
   return (
-    <div className={`${styles.fontSelector} text-light`}>
+    <div
+      className={`${styles.fontSelector} ${
+        theme === "light" ? "text-light" : "text-dark"
+      }`}
+    >
       <p>Sans Serif</p>
-      <div className={`${styles.fonts}`}>
+      <div
+        className={`${styles.fonts} ${
+          theme === "light" ? styles.font_light : styles.font_dark
+        }`}
+      >
         <p className="sans-Serif">Sans Serif</p>
         <p className="serif">Serif</p>
         <p className="mono">Mono</p>
