ee595ba82eae2b46317c4b114534bc979ab7d2f3
Ashish Dungdung <official.ashishd2@gmail.com>
2024-10-18 21:33:22 +0530

    feat(FontSelector): Added logic to display font selector menu when btn is clicked

--- src/components/buttons/fontSelector/FontSelector.tsx
+++ src/components/buttons/fontSelector/FontSelector.tsx
@@ -1,4 +1,5 @@
 import styles from "./FontSelector.module.scss";
+import { useState } from "react";
 
 import { useTheme } from "../../../context/ThemeContext";
 
@@ -6,24 +7,33 @@ import { ReactComponent as ArrowLogo } from "./../../../assets/images/icon-arrow
 
 const FontSelector = function () {
   const { theme } = useTheme();
+  const [displayFonts, setDisplayFonts] = useState<boolean>(false);
+
+  const handleDisplayFonts = function () {
+    setDisplayFonts((prevValue) => !prevValue);
+  };
 
   return (
     <div
-      className={`${styles.fontSelector} ${
+      className={`${styles.fontSelector_container} ${
         theme === "light" ? "text-light" : "text-dark"
       }`}
     >
-      <p>Sans Serif</p>
+      <div className={`${styles.fontSelector}`} onClick={handleDisplayFonts}>
+        <p>Sans Serif</p>
+
+        <ArrowLogo />
+      </div>
+
       <div
         className={`${styles.fonts} ${
           theme === "light" ? styles.font_light : styles.font_dark
-        }`}
+        } ${displayFonts ? styles.active : ""}`}
       >
         <p className="sans-Serif">Sans Serif</p>
         <p className="serif">Serif</p>
         <p className="mono">Mono</p>
       </div>
-      <ArrowLogo />
     </div>
   );
 };
